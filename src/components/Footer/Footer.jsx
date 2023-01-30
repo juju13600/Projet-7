19bef930d90bbdf6bef14fa664e06a42d3ac485c
juju13600 <justine.usseglio@hotmail.fr>
2023-01-30 16:22:59 +0100

    Insertion du Header et du Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
+import "./Footer.css";
+import logo from "../../assets/bannerFooter.png";
+
 function Footer() {
+  // récupération de l'année en cours
+  const actualYear = new Date().getFullYear();
   return (
     <footer>
       <div className="footer-container">
-       <h1>Youhou</h1>
+        <div className="footer-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="footer-rights">
+          <p>&copy; {actualYear} Kasa. All rights reserved</p>
+        </div>
       </div>
     </footer>
   );
 }
-
-
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,34 @@
+import logo from "../../assets/LOGO.png";
 import { NavLink } from "react-router-dom";
+
+import "./Header.css";
 function Header() {
   return (
     <nav className="nav">
       <NavLink to="/">
-      return <h1>Header</h1>
+        <img src={logo} alt="Logo" className="nav__img" />
       </NavLink>
+      {/* menu de navigation*/}
+      <div>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "nav__link active" : "nav__link"
+          }
+          to="/"
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "nav__link active" : "nav__link"
+          }
+          to="/about"
+        >
+          À Propos
+        </NavLink>
+      </div>
     </nav>
   );
 }
+
 export default Header;
